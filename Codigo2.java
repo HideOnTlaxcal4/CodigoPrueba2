db36f497cafc2a4328a0b01cc6202b8a24f83d5a
eduardo <edu4yh@gmail.com>
2024-12-16 19:21:23 -0600

    Commit final: Método completo funcional

--- Codigo2.java
+++ Codigo2.java
@@ -4,12 +4,14 @@ class Solution {
         int left = 0, maxLength = 0;
 
         for (int right = 0; right < s.length(); right++) {
-            count[s.charAt(right)]++; // Incrementa el conteo del carácter
+            count[s.charAt(right)]++;
 
-            while (count[s.charAt(right)] > 1) { // Si hay duplicado
-                count[s.charAt(left)]--; // Disminuye el conteo
-                left++; // Mueve la ventana hacia la derecha
+            while (count[s.charAt(right)] > 1) {
+                count[s.charAt(left)]--;
+                left++;
             }
+
+            maxLength = Math.max(maxLength, right - left + 1);
         }
 
         return maxLength;
