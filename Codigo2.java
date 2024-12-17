ad3210f1a032cc128f6550094e7cf6c8a4911cd7
eduardo <edu4yh@gmail.com>
2024-12-16 19:16:21 -0600

    Primer commit: Estructura principal

---
+++ Codigo2.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int[] count = new int[128];
+        int left = 0, maxLength = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+            // Lógica del conteo se añadirá después
+        }
+
+        return maxLength;
+    }
+}
