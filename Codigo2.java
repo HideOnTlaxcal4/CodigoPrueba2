245cb3cc0446ccc8e580e4098d939db9c34d6cfb
eduardo <edu4yh@gmail.com>
2024-12-16 19:20:39 -0600

    Tercer commit: logica para mover el puntero

--- Codigo2.java
+++ Codigo2.java
@@ -5,6 +5,11 @@ class Solution {
 
         for (int right = 0; right < s.length(); right++) {
             count[s.charAt(right)]++; // Incrementa el conteo del carácter
+
+            while (count[s.charAt(right)] > 1) { // Si hay duplicado
+                count[s.charAt(left)]--; // Disminuye el conteo
+                left++; // Mueve la ventana hacia la derecha
+            }
         }
 
         return maxLength;
